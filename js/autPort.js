3168f0efadf1fb508c21ba0a686dabc1ccb158a6
Angela Lima <angelamcostalima@icloud.com>
2023-03-30 21:33:10 +0100

    :bug: fix env variables fetching

--- js/autPort.js
+++ js/autPort.js
@@ -1,10 +1,10 @@
+let AUTH_CODE;
 const AUTH_URL = 'https://api.intra.42.fr/oauth/authorize';
 const TOKEN_URL = 'https://api.intra.42.fr/oauth/token';
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
 const SCOPES = 'public profile projects tig'
-let AUTH_CODE;
+const CLIENT_ID = window.MY_APP_CONFIG.clientId;
+const CLIENT_SECRET = window.MY_APP_CONFIG.clientSecret;
+const REDIRECT_URI = window.MY_APP_CONFIG.redirectUri;
 
 function login()
 {
