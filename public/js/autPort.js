bc96dac275e73838799d5000f4a9666c3209d4d6
Angela Lima <angelamcostalima@icloud.com>
2023-03-31 19:04:42 +0100

    :rocket: update server

--- public/js/autPort.js
+++ public/js/autPort.js
@@ -1,28 +1,15 @@
-let CLIENT_ID;
-let CLIENT_SECRET;
-let REDIRECT_URI;
-
-function login() {
-	const SCOPES = 'public profile projects tig';
-	const AUTH_URL = 'https://api.intra.42.fr/oauth/authorize';
-
-	window.location.href = `${AUTH_URL}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=${SCOPES}`
-}
-
-async function getCredentials()
+async function getCookies()
 {
-	try
-	{
-		const response = await fetch('/credentials');
-        const credentials = await response.text();
-		const parsedCredentials = JSON.parse(credentials.trim());
-        CLIENT_ID = parsedCredentials.clientId;
-        CLIENT_SECRET = parsedCredentials.clientSecret;
-        REDIRECT_URI = parsedCredentials.redirectUri;
-		console.log("success!");
-	} catch (error) {
-		console.log("ERROR: " + error);
-	}
+	if (!document.cookie.includes('access_token'))
+		window.location.href = '/callback';
+	console.log("success!");
 }
 
-getCredentials();
+function getIntraId(event)
+{
+	event.preventDefault();
+	const intraUser = $('#intra-user').val();
+	window.location.href = '/search/' + intraUser;
+}
+
+getCookies();
--- server/server.js
+++ server/server.js
@@ -1,15 +1,46 @@
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
+const cookieParser = require('cookie-parser');
 const app = express();
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
-const redirectUri = process.env.REDIRECT_URI;
 
 app.use(express.static('../public'));
+app.use(cookieParser());
 
-app.get('/credentials', (req, res) => {
-	res.json({ 'clientId': clientId, 'clientSecret': clientSecret, 'redirectUri': redirectUri })
+app.get('/callback', async (req, res) => {
+	try {
+		const response = await axios.post('https://api.intra.42.fr/oauth/token', {
+			grant_type: 'client_credentials',
+			client_id: clientId,
+			client_secret: clientSecret
+		});
+
+		res.cookie('access_token', response.data.access_token);
+		res.redirect('/') 
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('An error occurred');
+	}
+})
+
+app.get('/search/:intraUser', async (req, res) => {
+	const intraUser = req.params.intraUser;
+	const access_token = req.cookies.access_token;
+
+	try {
+		const response = await axios.get(`https://api.intra.42.fr/v2/users/${intraUser}`, {
+			headers: {
+				Authorization: `Bearer ${access_token}`
+			}
+		});
+
+		res.send(response.data);
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('An error occurred');
+	}
 })
 
 app.listen(3000, () => {
