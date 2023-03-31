0945300c1c0e5f576be6eea227bf6cd545ffc7eb
Angela Lima <angelamcostalima@icloud.com>
2023-03-31 16:44:31 +0100

    :rocket: update server.js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
 const app = express();
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const redirectUri = process.env.REDIRECT_URI;
 
-app.get('/', (req, res) => {
-	res.send('Hello easter hackaton =^.^=');
-})
+app.use(express.static('../public'));
 
 app.get('/credentials', (req, res) => {
-	const clientId = process.env.CLIENT_ID;
-	const clientSecret = process.env.CLIENT_SECRET;
-	const redirectUri = process.env.REDIRECT_URI;
-
-	res.json({clientId, clientSecret, redirectUri})
+	res.json({ 'clientId': clientId, 'clientSecret': clientSecret, 'redirectUri': redirectUri })
 })
 
 app.listen(3000, () => {
